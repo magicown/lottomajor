b319712ba54ba3c45b090af68705423ba5122e65
Annica2011 <phpprogram47@gmail.com>
2019-09-10 10:46:12 +0900

    Update dbconfig.php
    DB User data and password delete!

--- www/data/dbconfig.php
+++ www/data/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
 define('G5_MYSQL_HOST', 'localhost');
-define('G5_MYSQL_USER', 'lotto');
-define('G5_MYSQL_PASSWORD', 'jun1126k!');
-define('G5_MYSQL_DB', 'lottomajor');
+define('G5_MYSQL_USER', '');
+define('G5_MYSQL_PASSWORD', '');
+define('G5_MYSQL_DB', '');
 define('G5_MYSQL_SET_MODE', true);
 
 define('G5_TABLE_PREFIX', 'g5_');
